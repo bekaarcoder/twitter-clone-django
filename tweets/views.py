48de846c41fb2f2254cb8cc6d6325354b7b92fa9
Shashank <s_shashank@outlook.com>
2020-12-05 23:46:31 +0530

    added tailwind and template

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
+from .models import Tweet
 
-# Create your views here.
+def home_view(request, *args, **kwargs):
+  # return HttpResponse("<h1>Hello Django!</h1>")
+  return render(request, 'pages/home.html', context={}, status=200)
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):  
+  data = {
+    "id": tweet_id
+  }
+  status = 200
+  try:
+    obj = Tweet.objects.get(id=tweet_id)
+    data['content'] = obj.content
+  except:
+    data['message'] = "Tweet not found."
+    status = 404
+  return JsonResponse(data, status=status)
