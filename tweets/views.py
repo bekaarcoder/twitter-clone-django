d8a0f2d1716c3b4108d1335a7d69a5aa6b289121
Shashank <s_shashank@outlook.com>
2020-12-06 21:58:44 +0530

    loading tweets using javascript

--- tweets/views.py
+++ tweets/views.py
@@ -6,6 +6,14 @@ def home_view(request, *args, **kwargs):
   # return HttpResponse("<h1>Hello Django!</h1>")
   return render(request, 'pages/home.html', context={}, status=200)
 
+def tweet_list_view(request, *args, **kwargs):
+  tweets = Tweet.objects.all()
+  tweets_list = [{"id": x.id, "content": x.content} for x in tweets]
+  data = {
+    "response": tweets_list
+  }
+  return JsonResponse(data)
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):  
   data = {
     "id": tweet_id
