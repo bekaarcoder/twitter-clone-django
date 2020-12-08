adf4c24f41b429cfbcd493a4e5f53bedb3979e6b
Shashank <s_shashank@outlook.com>
2020-12-08 23:20:24 +0530

    django rest framework

--- djangoTwitter/settings.py
+++ djangoTwitter/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 LOGIN_URL = '/login'
+MAX_LENGTH_TWEET = 240
 
 
 # Application definition
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'tweets'
 ]
 
---
+++ tweets/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Tweet
+from django.conf import settings
+
+MAX_LENGTH = settings.MAX_LENGTH_TWEET
+
+class TweetSerializer(serializers.ModelSerializer):
+  
+  class Meta:
+    model = Tweet
+    fields = ['content']
+
+  def validate_content(self, value):
+    if len(value) > MAX_LENGTH:
+      raise serializers.ValidationError("Tweet is too long.")
+    return value
