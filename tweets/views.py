7b2a46faf199502d31496b62ce36e951d3adb721
Shashank <s_shashank@outlook.com>
2020-12-08 20:01:49 +0530

    rendering error message

--- tweets/views.py
+++ tweets/views.py
@@ -14,6 +14,7 @@ def home_view(request, *args, **kwargs):
 def tweet_create_view(request, *args, **kwargs):
   form = TweetForm(request.POST or None)
   next_url = request.POST.get('next') or None
+
   if form.is_valid():
     obj = form.save(commit=False)
     obj.save()
@@ -22,6 +23,11 @@ def tweet_create_view(request, *args, **kwargs):
     if(next_url != None and is_safe_url(next_url, ALLOWED_HOSTS)):
       return redirect(next_url)
     form = TweetForm()
+
+  if form.errors:
+    if request.is_ajax():
+      return JsonResponse(form.errors, status=400)
+
   return render(request, 'components/form.html', context={"form": form})
 
 def tweet_list_view(request, *args, **kwargs):
